489df2d4cd0d6a624b1d83314f18fbd60378fb44
Paul <Oskiman@users.noreply.github.com>
2025-02-20 15:58:18 +0000

    Add files via upload
    moved sort code into it's own function

--- bubsort.c
+++ bubsort.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
+void bubble_sort(int array[], int array_length);
 void swap(int *x, int *y);
 void print_array(int array_length, int array[]);
 
@@ -10,21 +11,25 @@ int main(void)
 {
 	int unsorted_array[10] = {10, 3, 5, 1, 2, 9, 4, 7, 6, 8};		// array to sort
 	int array_length = sizeof(unsorted_array) / sizeof(unsorted_array[0]);	// get length of array
-
-	for(int i = 0; i < array_length; i++)
-	{
-		for(int j = 0; j < (array_length - 1); j++)
-		{
-			if(unsorted_array[j] > unsorted_array[j + 1])
-			{
-				swap(&unsorted_array[j], &unsorted_array[j + 1]);
-			}
-		}
-	}
-
+	bubble_sort(unsorted_array, array_length);
 	print_array(array_length, unsorted_array);
 
 	return 0;
+}
+
+void bubble_sort(int array[], int array_length)
+{
+	
+	for(int i = 0; i < array_length; i++)
+	{
+		for(int j = 0; j < (array_length - i - 1); j++) // after each pass, highest value will be in correct place
+		{						// with -i we reduce the length of the search by 1 each pass
+			if(array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+			}
+		}
+	}
 }
 
 void swap(int *x, int *y)
