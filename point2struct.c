31e6440773f834793174debc2f320306ae26fc72
Paul <Oskiman@users.noreply.github.com>
2025-04-08 14:33:51 +0100

    Add files via upload

---
+++ point2struct.c
@@ -0,0 +1,24 @@
+// pointer to a struct
+#include <stdio.h>
+
+typedef struct test{
+	int x;
+	int y;
+	int z;
+}test;
+
+int main(void)
+{
+	// use dot(.) operator
+	test t1 = {1, 2, 3};
+	printf("x: %d\n", t1.x);
+
+	// use arrow (->) operator
+	test *ptr_to_t1 = &t1;
+	printf("y: %d\n", ptr_to_t1->y);
+
+	// use dereference(*) and dot(.) operators
+	printf("z: %d\n", (*ptr_to_t1).z);
+
+	return 0;
+}
