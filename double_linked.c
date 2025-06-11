8cb983cd55af090dc2a9381ac610d6c4fb1fd3eb
Paul <Oskiman@users.noreply.github.com>
2025-06-11 15:50:52 +0100

    Add files via upload

---
+++ double_linked.c
@@ -0,0 +1,72 @@
+// double linked list practice
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node* prev;
+	struct node* next;
+} node_t;
+
+int main(void)
+{
+	//create head of linked list
+	node_t* head = NULL;
+	head = (node_t*)malloc(sizeof(node_t));
+	if(head == NULL)
+	{
+		return 1;
+	}
+
+	head->data = 1;
+	head->next = NULL;
+
+	// create tail of linked list
+	node_t* tail = NULL;
+	tail = (node_t*)malloc(sizeof(node_t));
+	if(tail == NULL)
+	{
+		return 1;
+	}
+
+	tail->data = 3;
+	//tail->prev = head;
+	tail->next = NULL;
+
+	// join head & tail
+	head->next = tail;
+	tail->prev = head;
+
+	// output head & tail data values
+	node_t* current = head;		// create node to walk the list
+	while(current != NULL)
+	{
+		printf("%d\n", current->data);
+		current = current->next;
+	}
+
+	// insert a node inbetween head & tail
+	node_t* middle = NULL;
+	middle = (node_t*)malloc(sizeof(node_t));
+	if(middle == NULL)
+	{
+		return 1;
+	}
+
+	middle->data = 2;
+	head->next = middle;
+	middle->prev = head;
+	middle->next = tail;
+	tail->prev = middle;
+
+	// output complete list
+	current = head;		//reuse previously created node
+	while(current != NULL)
+	{
+		printf("%d\n", current->data);
+		current = current->next;	// move to next node
+		
+	}
+
+	return 0;
+}
