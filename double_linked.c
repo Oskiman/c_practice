ec8301301a5067fbbe495209c31829f585a44e18
Paul <Oskiman@users.noreply.github.com>
2025-06-16 16:00:32 +0100

    Add files via upload

--- double_linked.c
+++ double_linked.c
@@ -10,25 +10,31 @@ typedef struct node{
 
 void print_forward(node_t* node);
 void print_reverse(node_t* node);
-node_t* create_node(int data, node_t* next);
-node_t* add_to_list(node_t* node, int data);
+node_t* create_node(int data, node_t* prev, node_t* next);
+node_t* add_to_list(int data, node_t* node, node_t* prev, node_t* next);
 void remove_node(node_t* node);
 void free_list(node_t* node);
 
-// TODO: function to remove a given node
-// TODO: print list in reverse
+//TODO: remove_node();
 
 int main(void)
 {
-	// create a list
-	node_t* list = create_node(1, create_node(2, create_node(3, NULL)));
+	// create individual nodes
+	node_t* head = create_node(1, NULL, NULL);
+	node_t* middle = create_node(2, NULL, NULL);
+	node_t* tail = create_node(3, NULL, NULL);
 
-	print_forward(list);
+	// add nodes to list
+	add_to_list(1, head, NULL, middle);	
+	add_to_list(2, middle, head, tail);
+	add_to_list(3, tail, middle, NULL);
+
+	print_forward(head);
 	printf("\n");
-	print_reverse(list);
+	print_reverse(tail);
 
 	// remember to free nodes
-	free_list(list);
+	free_list(head);
 
 	return 0;
 }
@@ -62,7 +68,7 @@ void print_reverse(node_t* node)	// need to pass last node
 	printf("\n");
 }
 
-node_t* create_node(int data, node_t* next)
+node_t* create_node(int data, node_t* prev, node_t* next)
 {
 	node_t* node = NULL;
 	node = (node_t*)malloc(sizeof(node_t));
@@ -93,3 +99,11 @@ void free_list(node_t* node)
 		free(current);
 	}
 }
+
+node_t* add_to_list(int data, node_t* node, node_t* prev, node_t* next)
+{
+	node->data = data;
+	node->prev = prev;
+	node->next = next;
+}
+
