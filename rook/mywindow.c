c23d32a39a2e41b767e376ff9638da9494c86980
Paul <Oskiman@users.noreply.github.com>
2025-06-23 12:37:20 +0100

    Add files via upload

---
+++ rook/mywindow.c
@@ -0,0 +1,14 @@
+// trying to code x11 window stuff from scratch (with help as I am learning!)
+// compile with X11 flag
+// cc mywindow.c -lX11
+#include <X11/Xlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>	// for sleep()
+
+int main(void)
+{
+
+
+	return 0;
+}
