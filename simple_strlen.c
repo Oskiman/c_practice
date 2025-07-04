c935d3e5a46d61499518dffe3755e5cac434118b
Paul <Oskiman@users.noreply.github.com>
2025-07-04 10:59:51 +0100

    Add files via upload

--- simple_strlen.c
+++ simple_strlen.c
@@ -6,7 +6,12 @@ int simple_strlen(char* ptr);
 int main(void)
 {
 	char phrase[] = "Hello World!";
-	printf("phrase[] has a length of %d\n", simple_strlen(phrase));
+	printf("phrase[] has a length of %d characters.\n", simple_strlen(phrase));
+
+	char input_phrase[100];	// arbitrary length, I know it's not ideal, it'll do for now
+	printf("Enter a phrase: ");
+	fgets(input_phrase, sizeof(input_phrase), stdin);	// need to -1 for correct size
+	printf("Your input has a length of %d characters.\n", simple_strlen(input_phrase));
 
 	return 0;
 }
