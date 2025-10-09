c371c9aaf4aea411bea3478e61cc6297f9eaacdd
Paul <Oskiman@users.noreply.github.com>
2025-10-09 16:33:39 +0100

    Add files via upload

--- linked_list.c
+++ linked_list.c
@@ -7,15 +7,21 @@ typedef struct node{
 	struct node* next;
 } node_t;
 
+typedef struct head_tail{
+	struct node* next;
+} head_tail;
+
 void free_list(node_t* node);
 void output_list(node_t* node);
 node_t* create_node(int data);
+node_t* create_head_tail();
 
 int main(void)
 {
+	// could create a "special" node for head & tail containing only next pointer
 	//create head of linked list
 	node_t* head = create_node(0);
-
+	//node_t* head = create_head_tail();
 	// create first node in list for head to point to
 	node_t* node1 = create_node(1);
 	node1->next = NULL;
@@ -30,10 +36,10 @@ int main(void)
 	// join node1 to node3
 	node1->next = node3;
 
-	// output head & tail data values
+	// output node1 & node3 data values
 	output_list(head);
 
-	// insert a node inbetween head & tail
+	// insert a node inbetween node1 & node3
 	node_t* node2 = create_node(2);
 	node1->next = node2;
 	node2->next = node3;
@@ -45,7 +51,6 @@ int main(void)
 
 	return 0;
 }
-
 
 void free_list(node_t* node)
 {
@@ -60,18 +65,17 @@ void free_list(node_t* node)
 
 void output_list(node_t* node)
 {
-	// output complete list
 	node_t* current = node;
 	while(current != NULL)
 	{
 		if(current->data == 0)
 		{
-			current = current->next;	// we avoid outputting the 0 from the head node
+			current = current->next;	// skip to the next node so we avoid outputting the 0 from the head node
 		}
 		else
 		{
 			printf("%d\n", current->data);
-			current = current->next;	// move to next node
+			current = current->next;
 		}
 	}	
 }
@@ -83,6 +87,7 @@ node_t* create_node(int data)
 	node = (node_t*)malloc(sizeof(node_t));
 	if(node == NULL)
 	{
+		printf("Unable to create node!\n");
 		exit(1);
 	}
 	else
@@ -90,4 +95,17 @@ node_t* create_node(int data)
 		node->data = data;
 		return node;
 	}
+}
+
+node_t* create_head_tail()
+{	
+	node_t* node = NULL;
+	node = (node_t*)malloc(sizeof(node_t));
+	if(node == NULL)
+	{
+		printf("Unable to create node!\n");
+		exit(1);
+	}
+	else
+		return node;
 }
